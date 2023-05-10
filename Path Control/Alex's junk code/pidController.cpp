d9abf44f9e442703dab31729d7420d34f107f196
Nyctolex <65441185+Nyctolex@users.noreply.github.com>
2023-05-10 10:36:34 +0300

    updating the PID class

--- Path Control/Alex's junk code/pidController.cpp
+++ Path Control/Alex's junk code/pidController.cpp
@@ -1,54 +1,47 @@
-#include <iostream>
-
 class PIDController {
 public:
-    PIDController(float kp, float ki, float kd, float Ts, float alpha)
-        : m_kp(kp)
-        , m_ki(ki)
-        , m_kd(kd)
-        , m_Ts(Ts)
-        , m_alpha(alpha)
-    {}
+    PIDController(float kp, float ki, float kd) 
+        : Kp(kp), Ki(ki), Kd(kd), integral(0), prev_error(0), prev_derivative(0), filtered_derivative(0) {}
 
     float calculate(float setpoint, float process_variable) {
+        // Calculate time since last sample
+        unsigned long current_time = millis();
+        float dt = (current_time - last_time) / 1000.0; // convert to seconds
+        last_time = current_time;
+
+        // Update Ts
+        Ts = dt;
+
         // Calculate error
         float error = setpoint - process_variable;
 
         // Calculate integral term
-        m_integral += error * m_Ts;
+        integral += error * Ts;
 
         // Calculate derivative term with FIR filter
-        m_derivative = (error - m_prev_error) / m_Ts;
-        m_filtered_derivative = m_alpha * m_derivative + (1 - m_alpha) * m_prev_derivative;
+        derivative = (error - prev_error) / Ts;
+        filtered_derivative = alpha * derivative + (1 - alpha) * prev_derivative;
 
         // Calculate control signal
-        float control_signal = m_kp * error + m_ki * m_integral + m_kd * m_filtered_derivative;
+        float control_signal = Kp * error + Ki * integral + Kd * filtered_derivative;
 
         // Store previous error and derivative for next iteration
-        m_prev_error = error;
-        m_prev_derivative = m_filtered_derivative;
+        prev_error = error;
+        prev_derivative = filtered_derivative;
 
         return control_signal;
     }
 
 private:
-    float m_kp;
-    float m_ki;
-    float m_kd;
-    float m_Ts;
-    float m_alpha;
-    float m_integral = 0.0;
-    float m_prev_error = 0.0;
-    float m_prev_derivative = 0.0;
-    float m_derivative = 0.0;
-    float m_filtered_derivative = 0.0;
+    const float Kp;
+    const float Ki;
+    const float Kd;
+    const float alpha = 0.1;
+    float integral;
+    float prev_error;
+    float prev_derivative;
+    float derivative;
+    float filtered_derivative;
+    float Ts = 0.01;
+    unsigned long last_time = millis();
 };
-
-
-
-
-
-int main() {
-    std::cout << "Hello, world!\n";
-    return 0;
-}
